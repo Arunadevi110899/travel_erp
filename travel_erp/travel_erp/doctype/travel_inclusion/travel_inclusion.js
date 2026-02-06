813e7cc958cef2357fca84774a0507a602f679ff
Sriram Annamalai <sriramannamalai2002@gmail.com>
2026-02-06 22:23:48 +0530

    Complete the Structure for Package

---
+++ travel_erp/travel_erp/doctype/travel_inclusion/travel_inclusion.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Techinsights and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Travel Inclusion", {
+// 	refresh(frm) {
+
+// 	},
+// });
